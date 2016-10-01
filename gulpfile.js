651d54dc5252d9b9aba24ddca1ad08d46870a21c
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2016-10-01 10:19:45 +0900

    jekyll installed

--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,72 @@ var markdownpdf = require('gulp-markdown-pdf');
 var markdown = require('gulp-markdown');
 var docco = require("gulp-docco");
 var ghPages = require('gulp-gh-pages');
-
+var browserSync = require('browser-sync');
+var sass        = require('gulp-sass');
+var prefix      = require('gulp-autoprefixer');
+var cp          = require('child_process');
 
 gulp.task('pdf', function() {
     gulp.src('./*.md')
                  .pipe(markdownpdf())
                  .pipe(gulp.dest('build/pdf'));
 });
+
+/**
+ * Build the Jekyll Site
+ */
+var messages = {
+    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+};
+
+gulp.task('jekyll-build', function (done) {
+    browserSync.notify(messages.jekyllBuild);
+    return cp.spawn( "jekyll" , ['build'], {stdio: 'inherit'})
+        .on('close', done);
+});
+
+/**
+ * Rebuild Jekyll & do page reload
+ */
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+    browserSync.reload();
+});
+
+/**
+ * Wait for jekyll-build, then launch the Server
+ */
+gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
+    browserSync({
+        server: {
+            baseDir: '_site'
+        }
+    });
+});
+
+/**
+ * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
+ */
+gulp.task('sass', function () {
+    return gulp.src('_scss/main.scss')
+        .pipe(sass({
+            includePaths: ['scss'],
+            onError: browserSync.notify
+        }))
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(gulp.dest('_site/css'))
+        .pipe(browserSync.reload({stream:true}))
+        .pipe(gulp.dest('css'));
+});
+
+/**
+ * Watch scss files for changes & recompile
+ * Watch html/md files, run jekyll & reload BrowserSync
+ */
+gulp.task('watch', function () {
+    gulp.watch('_scss/*.scss', ['sass']);
+    gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
+});
+
 
 gulp.task('doc', function() {
     return gulp.src('index.md')
